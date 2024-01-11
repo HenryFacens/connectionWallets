19acbc0fe4b2951897e7fd9423ca2377d4b9d62e
| X _ X | <104470074+HenryFacens@users.noreply.github.com>
2024-01-11 09:34:32 -0300

    feat:All things

--- app/connection/views.py
+++ app/connection/views.py
@@ -1,3 +1,11 @@
+from django.views import View
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+class Home(View):
+
+    def get(self, request):
+        return render(request, 'home/login.html')
+    
+    def post(self, request):
+        return JsonResponse({'message': 'Hello, AJAX!'})
